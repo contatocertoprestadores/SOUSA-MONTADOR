c3536011a3849981b63ced9e7d5b50ae1353e642
Contato Certo SP New <contatocerto.prestadores@gmail.com>
2026-09-16 21:09:05 -0300

    Add files via upload

---
+++ app/layout.js
@@ -0,0 +1,18 @@
+import './globals.css'
+import Tracker from '../components/Tracker'
+
+export const metadata = {
+  title: 'Sousa Montagens - Montador de Móveis Premium | Assis e Tarumã',
+  description: 'Montador de móveis em Assis - Sem pagamento antecipado',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <Tracker />
+        {children}
+      </body>
+    </html>
+  )
+}
